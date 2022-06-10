8d443f8915d2090e7823bda2c56e30557d0fd4d9
StevenLi007 <105518183+lszhssh@users.noreply.github.com>
2022-06-10 08:33:02 -0700

    Complete LockingBT

--- LockingBT.java
+++ LockingBT.java
@@ -11,3 +11,73 @@
 // You may augment the node to add parent pointers or any other property you would like.
 // You may assume the class is used in a single-threaded program, so there is no need 
 // for actual locks or mutexes. Each method should run in O(h), where h is the height of the tree.
+
+public class LockingBT {
+    static class Node {
+        int value;
+        boolean isLocked;
+        Node parent;
+        Node leftChild;
+        Node rightChild;
+
+        public Node(int value) {
+            this.value = value;
+            this.isLocked = false;
+        }
+    }
+
+    Node root;
+    public LockingBT(int value) {
+        this.root = new Node(value);
+    }
+
+    public boolean isLocked(Node node) {
+        return node.isLocked;
+    }
+
+    public boolean lock(Node node) {
+        if (checkAncestors(node) && checkDescendants(node)) {
+            node.isLocked = true;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean unlock(Node node) {
+        if (checkAncestors(node) && checkDescendants(node)) {
+            node.isLocked = false;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkAncestors(Node node) {
+        while (node.parent != null) {
+            if (node.parent.isLocked) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean checkDescendants(Node node) {
+        if (node == null) {
+            return true;
+        }
+        if (node.leftChild == null && node.rightChild == null) {
+            if (node.isLocked) {
+                return false;
+            }
+            return true;
+        }
+        return checkDescendants(node.leftChild) && checkDescendants(node.rightChild);
+    }
+    
+    public static void main(String[] args) {
+        LockingBT bt = new LockingBT(1);
+        bt.root.leftChild = new Node(2);
+        bt.root.rightChild = new Node(3);
+        System.out.println(bt.lock(bt.root));
+        System.out.println(bt.isLocked(bt.root));
+    }
+}
