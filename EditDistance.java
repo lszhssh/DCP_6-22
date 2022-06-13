8498a3568043ca80d3751d595a854155bd250246
StevenLi007 <105518183+lszhssh@users.noreply.github.com>
2022-06-13 14:59:45 -0700

    Learned RegExprMatching

---
+++ EditDistance.java
@@ -0,0 +1,7 @@
+// 6/13/22
+
+public class EditDistance {
+    public static int editDist(String s1, String s2) {
+        return 0;
+    }
+}
--- RegExprMatching.java
+++ RegExprMatching.java
@@ -1,33 +1,41 @@
 // 6/7/22
 
 public class RegExprMatching {
-    public static boolean onlyPeriods(String regExpr, String str) {
-        int start = 0;
-        int end = 0;
-        while (regExpr.indexOf(".") != -1) {
-            end = regExpr.indexOf(".");
-            if (regExpr.substring(start, end).equals(str.substring(start, end))) {
-                if (regExpr.charAt(end) == str.charAt(end)) {
-                    start = end + 1;
-                } else {
-                    return false;
+    public static boolean matchFirstChar(String str, String regex) {
+        return str.charAt(0) == regex.charAt(0) || (regex.charAt(0) == '.' && str.length() > 0);
+    }
+
+    public static boolean match(String str, String regex) {
+        if (regex.isEmpty()) {
+            return str.isEmpty();
+        }
+        if (regex.length() == 1 || regex.charAt(1) != '*') {
+            if (matchFirstChar(str, regex)) {
+                return match(str.substring(1), regex.substring(1));
+            } else {
+                return false;
+            }
+        } else {
+            if (matchFirstChar(str, regex)) {
+                // for (int i = 0; i < str) how to check?
+                if (regex.length() == 2) {
+                    return true;
                 }
+                for (int i = 0; i < str.length() - 1; i++) {
+                    if (str.substring(i).equals(regex.substring(2))) {
+                        return true;
+                    }
+                }
+                return false;
             } else {
                 return false;
             }
         }
-        if (start >= regExpr.length() || start >= str.length()) {
-            
-        }
-        if (regExpr.substring(start).equals(str.substring(start))) {
-            return true;
-        }
-        return false;
     }
 
     public static void main(String[] args) {
-        String regExpr = "ra.";
-        String str = "ray";
-        System.out.println(RegExprMatching.onlyPeriods(regExpr, str));
+        String regex = ".*at";
+        String str = "chats";
+        System.out.println(RegExprMatching.match(str, regex));
     }
 }
