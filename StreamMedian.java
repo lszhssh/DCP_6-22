c3007f38d8243304b6f463c5043904a6ae5a8437
StevenLi007 <105518183+lszhssh@users.noreply.github.com>
2022-06-16 15:56:16 -0700

    Create StreamMedian.java

---
+++ StreamMedian.java
@@ -0,0 +1,13 @@
+// 6/15/22
+
+import java.util.PriorityQueue;
+
+public class StreamMedian {
+    public static int streamMedian(int[] stream) {
+        // create two heaps
+        // iter thru stream and add to the two heaps as required
+            // rebalance the heaps
+        // return the root of the larger heap or the average of the 
+        // roots if the heaps are the same size
+    }
+}
