865a5aa1448724f8406b0966355b1ebee19701c1
StevenLi007 <105518183+lszhssh@users.noreply.github.com>
2022-06-18 16:56:10 -0700

    Learned Houses

--- Houses.java
+++ Houses.java
@@ -2,27 +2,41 @@
 
 public class Houses {
     public static int minCost(int[][] costs) {
-        int minCost = 0;
-        for (int i = 0; i < costs[0].length; i++) {
-            int totalCost = costs[0][i];
-            int prevColorIdx = i;
-            for (int j = 1; j < costs.length; j++) {
-                int minHouseCost = 0;
-                // for (int )
+        int cheapest = 0;
+        int cheapestIdx = -1;
+        int secondCheapest = 0;
+        for (int i = 0; i < costs.length; i++) {
+            int newCheapest = Integer.MAX_VALUE;
+            int newCheapestIdx = -1;
+            int newSecondCheapest = Integer.MAX_VALUE;
+            for (int j = 0; j < costs[i].length; j++) {
+                int prevCheapest = j == cheapestIdx ? secondCheapest : cheapest;
+                int cost = prevCheapest + costs[i][j];
+                if (cost < newCheapest) {
+                    newSecondCheapest = newCheapest;
+                    newCheapest = cost;
+                    newCheapestIdx = j;
+                } else if (cost < newSecondCheapest) {
+                    newSecondCheapest = cost;
+                }
             }
+            cheapest = newCheapest;
+            cheapestIdx = newCheapestIdx;
+            secondCheapest = newSecondCheapest;
         }
+        return cheapest;
     }
 
-    // iter thru all possible starting points for the first house
-    // keep track of the color of the prev house and pick the cheapest cost for
-    // the house that's not the color of the prev house
-    // keep track of the best costs at each starting point and return the min
+    public static void main(String[] args) {
+        int[][] costs = new int[][] {{1, 1, 1}, {10, 8, 12}, {1000, 1, 1000}};
+        System.out.println(minCost(costs));
+    }
 }
 
-// 1    10    1000
-// 1    8     1
-// 1    12    1000
+// 1    1    1
+// 10   8    12
+// 1000 1    1000
 
 // starting at (0, 0) = 1 + 8 + 1000 = 1009
-// starting at (1, 0) = 1 + 10 + 1 = 12
-// starting at (2, 0) = 1 + 8 + 1000 = 1009
+// starting at (0, 1) = 1 + 10 + 1 = 12
+// starting at (0, 2) = 1 + 8 + 1000 = 1009
