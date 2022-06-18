60ef15544e046742f5cf807e541209cb10ab8428
StevenLi007 <105518183+lszhssh@users.noreply.github.com>
2022-06-18 08:52:14 -0700

    Optimized RGBSort

--- RGBSort.java
+++ RGBSort.java
@@ -1,13 +1,13 @@
 // 6/17/22
 
 public class RGBSort {
-    public static void swap(char[] letters, int currIdx, int currSortedIdx) {
-        char temp = letters[currSortedIdx];
-        letters[currSortedIdx] = letters[currIdx];
-        letters[currIdx] = temp;
+    public static void swap(char[] letters, int idx1, int idx2) {
+        char temp = letters[idx2];
+        letters[idx2] = letters[idx1];
+        letters[idx1] = temp;
     }
 
-    public static char[] rgbSort(char[] letters) {
+    public static void rgbSort(char[] letters) {
         int currSortedIdx = 0;
         for (int i = 0; i < letters.length; i++) {
             if (letters[i] == 'R') {
@@ -21,13 +21,31 @@ public class RGBSort {
                 currSortedIdx++;
             }
         }
-        return letters;
+    }
+
+    public static void optimRgbSort(char[] letters) {
+        int low = 0;
+        int mid = 0;
+        int high = letters.length - 1;
+        while (mid <= high) {
+            if (letters[mid] == 'R') {
+                swap(letters, low, mid);
+                low++;
+                mid++;
+            } else if (letters[mid] == 'G') {
+                mid++;
+            } else {
+                swap(letters, mid, high);
+                high--;
+            }
+        }
     }
 
     public static void main(String[] args) {
         char[] letters = new char[] {'G', 'B', 'R', 'R', 'B', 'R', 'G', 'B'};
         // char[] letters = new char[] {'R', 'R', 'R'};
-        RGBSort.rgbSort(letters);
+        // RGBSort.rgbSort(letters);
+        RGBSort.optimRgbSort(letters);
         for (char c : letters) {
             System.out.print(c + " ");
         }
