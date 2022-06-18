ad1f724b605666c20b9e469f8a0b1835506b0490
StevenLi007 <105518183+lszhssh@users.noreply.github.com>
2022-06-18 11:06:22 -0700

    Create SecondLargestElemBST.java

---
+++ SecondLargestElemBST.java
@@ -0,0 +1,7 @@
+// 6/18/22
+
+// Solutions: https://leetcode.com/problems/kth-smallest-element-in-a-bst/solution/
+
+public class SecondLargestElemBST {
+    // Approach 1: inorder traversal -> find 2nd largest (start from end of array)
+}
