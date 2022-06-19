ce1b0c6c7e39bec953e7c179100cf93028b600dd
StevenLi007 <105518183+lszhssh@users.noreply.github.com>
2022-06-19 09:24:24 -0700

    Learned MinStepsMaze

--- MinStepsMaze.java
+++ MinStepsMaze.java
@@ -1,19 +1,82 @@
-// 6/5/22:
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Queue;
 
-// You are given an M by N matrix consisting of booleans that represents a board. 
-// Each True boolean represents a wall. Each False boolean represents a tile you can walk on.
-// Given this matrix, a start coordinate, and an end coordinate, 
-// return the minimum number of steps required to reach the end coordinate from the start. 
-// If there is no possible path, then return null. You can move up, left, down, and right.
-// You cannot move through walls. You cannot wrap around the edges of the board.
+// 6/5/22
 
-// For example, given the following board:
-// [[f, f, f, f],
-// [t, t, f, t],
-// [f, f, f, f],
-// [f, f, f, f]]
-// and start = (3, 0) (bottom left) and end = (0, 0) (top left), 
-// the minimum number of steps required to reach the end is 7, 
-// since we would need to go through (1, 2) because there is a wall everywhere else on the second row.
+public class MinStepsMaze {
+    static class Pair {
+        int xCoord;
+        int yCoord;
+        int depth;
+        
+        public Pair(int xCoord, int yCoord, int depth) {
+            this.xCoord = xCoord;
+            this.yCoord = yCoord;
+            this.depth = depth;
+        }
+    }
 
-// Solution time: incomplete
+    public static boolean isValidTile(boolean[][] maze, Pair position) {
+        if (position.xCoord < 0 || position.xCoord >= maze.length ||
+                position.yCoord < 0 || position.yCoord >= maze[0].length) {
+            return false;
+        }
+        return !maze[position.xCoord][position.yCoord];
+    }
+
+    public static ArrayList<Pair> getValidNeighbors(boolean[][] maze, Pair currPosition) {
+        ArrayList<Pair> neighbors = new ArrayList<>();
+        int x = currPosition.xCoord;
+        int y = currPosition.yCoord;
+        int newDepth = currPosition.depth + 1;
+        Pair northNeighbor = new Pair(x, y - 1, newDepth);
+        if (isValidTile(maze, northNeighbor)) {
+            neighbors.add(northNeighbor);
+        }
+        Pair eastNeighbor = new Pair(x + 1, y, newDepth);
+        if (isValidTile(maze, eastNeighbor)) {
+            neighbors.add(eastNeighbor);
+        }
+        Pair southNeighbor = new Pair(x, y + 1, newDepth);
+        if (isValidTile(maze, southNeighbor)) {
+            neighbors.add(southNeighbor);
+        }
+        Pair westNeighbor = new Pair(x - 1, y, newDepth);
+        if (isValidTile(maze, westNeighbor)) {
+            neighbors.add(westNeighbor);
+        }
+        return neighbors;
+    }
+
+    public static int minStepsMaze(boolean[][] maze, Pair start, Pair end) {
+        Queue<Pair> tiles = new ArrayDeque<>();
+        HashSet<Pair> seen = new HashSet<>();
+        tiles.add(start);
+        while (!tiles.isEmpty()) {
+            Pair curr = tiles.poll();
+            seen.add(curr);
+            if (curr.xCoord == end.xCoord && curr.yCoord == end.yCoord) {
+                return curr.depth;
+            }
+            ArrayList<Pair> neighbors = getValidNeighbors(maze, curr);
+            for (Pair p : neighbors) {
+                if (!seen.contains(p)) {
+                    tiles.add(p);
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        boolean[][] maze = new boolean[][] {{false, false, false, false}, 
+                                            {true, true, false, true}, 
+                                            {false, false, false, false}, 
+                                            {false, false, false, false}};
+        Pair start = new Pair(3, 0, 0);
+        Pair end = new Pair(0, 0, 0);
+        System.out.println(minStepsMaze(maze, start, end));
+    }
+}
