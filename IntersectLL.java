70021a2c739f393bcefb630e378e721e891a3885
StevenLi007 <105518183+lszhssh@users.noreply.github.com>
2022-06-19 08:22:02 -0700

    Learned IntersectLL

--- IntersectLL.java
+++ IntersectLL.java
@@ -1,15 +1,4 @@
-// Problem from 6/2/22:
-
-// Given two singly linked lists that intersect at some point, 
-// find the intersecting node. The lists are non-cyclical.
-// For example, given A = 3 -> 7 -> 8 -> 10 and B = 99 -> 1 -> 8 -> 10, 
-// return the node with value 8.
-// In this example, assume nodes with the same value are 
-// the exact same node objects.
-// Do this in O(M + N) time (where M and N are 
-// the lengths of the lists) and constant space.
-
-// Solution time: incomplete
+// 6/2/22
 
 public class IntersectLL {
     static class Node {
@@ -22,37 +11,63 @@ public class IntersectLL {
     }
 
     Node root;
+    int size;
 
-    public IntersectLL(int val) {
-        this.root = new Node(val);
+    public IntersectLL(Node root) {
+        this.root = root;
+        size = 1;
     }
 
-    // Naive solution: for each node in the first list, see if that node appears
-    // in the second list (time: O(M*N), space: O(1))
+    public void add(Node node) {
+        Node currNode = this.root;
+        for (int i = 1; i < this.size; i++) {
+            currNode = currNode.next;
+        }
+        currNode.next = node;
+        this.size++;
+    }
 
-    // Optimized solution:
-    // - Singly LL => we can only move forward in the list => only move if we can 
-    // guarantee that that node isn't the intersect
-        // [] How do we know that unless we traverse the entire list?
-            // - Two high-level possibilities:
-                // 1. Manipulate the DS to find the answer in the desired time and space
-                    // - We can't make BSTs out of the SLLs because that wouldn't be O(1) space;
-                    // in any case, that would only reduce the time to O(M*logN)
-                    // - Serialize SLL into String would initially satisfy the time and space
-                    // constraints, but any further operations necessary to find the answer
-                    // would not satisfy those constraints
-                        // - Similarly, keeping a Hashtable of values and occurrences would
-                        // violate the desired space complexity
-                        // - If we add all Nodes into an ArrayList, we can solve the problem in 
-                        // O(M+N) time and O(M+N) space. This seems to be the best solution I've
-                        // thought of.
-                // 2. Some undiscovered algorithm that can answer the question without
-                // manipulating the DS
-                    // - Rare
-    // - O(1) space => only storing a variable, not another DS
+    public static int intersect(IntersectLL l1, IntersectLL l2) {
+        Node l1Pointer = l1.root;
+        Node l2Pointer = l2.root;
+        if (l1.size > l2.size) {
+            int diff = l1.size - l2.size;
+            for (int i = 0; i < diff; i++) {
+                l1Pointer = l1Pointer.next;
+            }
+        } else if (l1.size < l2.size) {
+            int diff = l2.size - l1.size;
+            for (int i = 0; i < diff; i++) {
+                l2Pointer = l2Pointer.next;
+            }
+        }
+        while (l1Pointer.val != l2Pointer.val) {
+            l1Pointer = l1Pointer.next;
+            l2Pointer = l2Pointer.next;
+        }
+        return l1Pointer.val;
+    }
 
     public static void main(String[] args) {
-        String sll1 = "3,7,8,10";
-        String[] s1 = new String[5];
+        Node n1 = new Node(5);
+        Node n2 = new Node(3);
+        Node n3 = new Node(7);
+        Node n4 = new Node(99);
+        Node n5 = new Node(1);
+        Node nX = new Node(8);
+        Node nY = new Node(10);
+
+        IntersectLL l1 = new IntersectLL(n1);
+        l1.add(n2);
+        l1.add(n3);
+        l1.add(nX);
+        l1.add(nY);
+
+        IntersectLL l2 = new IntersectLL(n4);
+        l2.add(n5);
+        l2.add(nX);
+        l2.add(nY);
+
+        System.out.println(intersect(l1, l2));
     }
 }
